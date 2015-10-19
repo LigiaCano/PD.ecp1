184ba807572d0bd66988e6c6b2948152e56de602
Adan <Adan@Adan-PC>
2015-10-20 01:37:19 +0200

    Listo patron Singleton

--- src/main/java/singleton/factory/ReferencesFactory.java
+++ src/main/java/singleton/factory/ReferencesFactory.java
@@ -3,17 +3,25 @@ package singleton.factory;
 import java.util.HashMap;
 import java.util.Map;
 
-public class ReferencesFactory {
+public final class ReferencesFactory {
+	
+	private static ReferencesFactory referencesFactory = new ReferencesFactory();
+	
     private Map<String, Integer> references;
 
     private int reference;
 
-    public ReferencesFactory() {
+    private  ReferencesFactory() {
         this.references = new HashMap<>();
         this.reference = 0;
     }
+    
+    public static ReferencesFactory getFactory() {
+    	return ReferencesFactory.referencesFactory;
+    }
 
     public int getReference(String key) {
+    	
         Integer result = this.references.get(key);
         if (result == null) {
             this.references.put(key, this.reference);
